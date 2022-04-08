a000b322581275f56500da8cf41af5f0160b3b71
cckj2580 <2295589137@qq.com>
2022-04-09 03:02:29 +0800

    青龙脚本：抖音极速版

---
+++ dyjs/dyjs_cookies.js
@@ -0,0 +1,16 @@
+//Cookie和x-Tt-Token去抓到的headers里搜
+let cookies = [
+    {
+        "name": "账号1",
+        "Cookie": "xxxxxxxxx",
+        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
+    },
+    {
+        "name": "账号2",
+        "Cookie": "xxxxxxxxx",
+        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
+    }
+
+];
+
+module.exports = cookies;
